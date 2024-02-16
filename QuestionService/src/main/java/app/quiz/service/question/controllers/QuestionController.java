c048d0bc2ec3476d879f3df4d7fde774c6b4461d
Ahsan Ali Mughal <mughalahsanali77@gmail.com>
2024-02-16 15:28:56 +0500

    changed return type in question controller

--- QuestionService/src/main/java/app/quiz/service/question/controllers/QuestionController.java
+++ QuestionService/src/main/java/app/quiz/service/question/controllers/QuestionController.java
@@ -1,8 +1,10 @@
 package app.quiz.service.question.controllers;
 
+import app.quiz.common.util.ResponseUtil;
 import app.quiz.core.dto.QuestionDto;
 import app.quiz.service.question.services.QuestionService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,23 +15,28 @@ import java.util.List;
 public class QuestionController {
     private final QuestionService questionService;
     @GetMapping
-    List<QuestionDto> get(){
-        return questionService.get();
+    ResponseEntity<Object> get(){
+        List<QuestionDto> dtos = questionService.get();
+        return ResponseUtil.returnResponse(dtos);
     }
     @GetMapping("/findById/{id}")
-    QuestionDto get(@PathVariable String id){
-        return questionService.get(id);
+    ResponseEntity<Object> get(@PathVariable String id){
+        QuestionDto questionDto = questionService.get(id);
+        return ResponseUtil.returnResponse(questionDto);
     }
     @PostMapping("/create")
-    QuestionDto create(@RequestBody QuestionDto dto){
-        return questionService.create(dto);
+    ResponseEntity<Object> create(@RequestBody QuestionDto dto){
+        QuestionDto questionDto = questionService.create(dto);
+        return ResponseUtil.returnResponse(dto);
     }
     @PutMapping("/update")
-    QuestionDto update(@RequestBody QuestionDto dto){
-        return questionService.update(dto);
+    ResponseEntity<Object> update(@RequestBody QuestionDto dto){
+        QuestionDto update = questionService.update(dto);
+        return ResponseUtil.returnResponse(update);
     }
     @DeleteMapping("/{id}")
-    QuestionDto delete(@PathVariable String id){
-        return questionService.delete(id);
+    ResponseEntity<Object> delete(@PathVariable String id){
+        QuestionDto delete = questionService.delete(id);
+        return ResponseUtil.returnResponse(delete);
     }
 }
